260245a9c8b93a3084a3f40b41b161c8329f1552
Settawat Jamsai <buagern@buataitom.com>
2014-03-18 11:17:17 +0700

    fixed config file

--- src/config/facebook.php
+++ src/config/facebook.php
@@ -2,8 +2,8 @@
 
 return array(
 
-	'appId'  => '637107483022418',
-	'secret' => '6b34d2909bd0e2ee5a09c16d83c76dc5',
+	'appId'  => '',
+	'secret' => '',
 	'scopes' => array()
 
 );
--- src/config/instagram.php
+++ src/config/instagram.php
@@ -2,8 +2,8 @@
 
 return array(
 
-	'client_id'  => 'cd8a357e2c6146dd9d6e385835c8d5e6',
-	'client_secret' => 'd72197b3ff72410289f637e424dbb901',
+	'client_id'  => '',
+	'client_secret' => '',
 	'redirect_uri' => '',
 	'scopes' => array()
 
